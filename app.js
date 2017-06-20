672ef9aec8a595b20ee37fa0421c8b6d918a2a09
TheFamousChineeMagician <mli007@hawaii.rr.com>
2017-06-19 14:58:06 -1000

    finished

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 function fortuneTeller(){
-  
-}
+  var numOfChildren = 69;
+  var partnersName = "KK";
+  var geographicalLocation = "California";
+  var jobTitle = "Military Computer Engineer";
 
+  console.log( "You will be a X in Y, and married to Z with N kids.");
+
+console.log("You will be a " + jobTitle + " in " + geographicalLocation + " , and married to " + partnersName + " with " + numOfChildren + " kids.");  
+}
+fortuneTeller();
 function ageCalculator(){
 
 }
